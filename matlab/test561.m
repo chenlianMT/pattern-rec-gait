7153ef1a6204584c123137df58c3e36393457e84
Raymond Xia <yangyanx@andrew.cmu.edu>
2015-12-06 18:28:24 -0500

    test561 finished
    - 84.2% accuracy with linear kernel SVM on raw features
    - 84.2% accuracy with linear kernel SVM on PCA projection of raw features

--- matlab/test561.m
+++ matlab/test561.m
@@ -4,32 +4,41 @@ clear;
 DOPCA = 1;
 DOLDA = 0;
 
-[TRAIN, TEST] = dataPreprocess_HAR(50);
-train_idx = (train_activity == 1);
-test_idx = (test_activity == 1);
-TRAIN = TRAIN'; TRAIN = TRAIN(:,train_idx);
-TEST = TEST'; TEST = TEST(:,test_idx);
-train_label = train_label(train_idx);
-test_label = test_label(test_idx);
+[TRAIN, TEST] = dataPreprocess_HAR(30,1);
+%train_idx = (train_activity == 1);
+%test_idx = (test_activity == 1);
+%TRAIN = TRAIN'; TRAIN = TRAIN(:,train_idx);
+%TEST = TEST'; TEST = TEST(:,test_idx);
+%train_label = train_label(train_idx);
+%test_label = test_label(test_idx);
+train_label = TRAIN.label_subject;
+test_label = TEST.label_subject;
+train = TRAIN.features_561;
+test = TEST.features_561;
 %%{
 if DOPCA,
-[vecs, lambdas, TRAINnew, meanX] = PCA(TRAIN);
+[vecs, lambdas, train_new, meanX] = PCA(train');
 [~,idx] = sort(lambdas,'descend');
 dim = sum(double(lambdas > 1));
-TRAINnew = TRAINnew(idx(1:dim),:);
+train_new = train_new(idx(1:dim),:);
 vecs = vecs(:,idx(1:dim));
-TESTnew = vecs' * (TEST-repmat(meanX,1,size(TEST,2)));
+train_new = train_new';
+test_new = (vecs' * (test'-repmat(meanX,1,size(test,1))))';
 end
 %}
 if DOLDA,
-[ vecs, lambdas ] = LDA(TRAIN, 30, train_label);
+[ vecs, lambdas ] = LDA(train', 30, train_label);
 [~,idx] = sort(lambdas,'descend');
 dim = sum((lambdas > 1));
-TRAINnew = TRAINnew(idx(1:dim),:);
 vecs = vecs(:,idx(1:dim));
-TESTnew = vecs' * TEST;
+train_new = (vecs' * train')';
+test_new = (vecs' * test')';
 end
 
+if ~DOPCA && ~DOLDA,
+    train_new = train;
+    test_new = test;
+end
 
-svm_model = svmtrain(train_label, TRAIN', '-c 1 -g 0.03');
-[predict_label, accuracy, ~] = svmpredict(test_label, TEST', svm_model);
+svm_model = svmtrain(train_label, train_new, '-t 0');
+[predict_label, accuracy, ~] = svmpredict(test_label, test_new, svm_model);
